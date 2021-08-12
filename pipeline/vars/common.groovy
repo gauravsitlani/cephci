88bb7ddebf6ac71acf434911f5b3acbc66dbd2d2
ckulal <83442614+ckulal@users.noreply.github.com>
2021-08-12 17:58:35 +0530

    Update common.groovy

--- pipeline/vars/common.groovy
+++ pipeline/vars/common.groovy
@@ -233,12 +233,12 @@ def fetchEmailBodyAndReceiver(def test_results, def isStage) {
     return ["to_list" : to_list, "jobStatus" : jobStatus, "body" : body]
 }
 
-def sendGChatNotification(def tier, def test_results, def isStage=true){
-    def params = fetchEmailBodyAndReceiver(test_results, isStage)
-    def jobStatus = params["jobStatus"]
-    msg= "Status of ${env.composeId}:${tier} is ${jobStatus}"
+def sendGChatNotification(def tier){
+    msg= "Run for ${env.composeId}:${tier} is done"
     googlechatnotification(url: "id:rhcephCIGChatRoom",
-                           message: ${msg})
+                           message: ${msg},
+                           notifySuccess: true,
+                           notifyFailure: true)
 }
 
 def sendEMail(def subjectPrefix, def test_results, def isStage=true) {
