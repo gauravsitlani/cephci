d63ef14e061ba635731f3b938942be8b370038b6
ckulal <83442614+ckulal@users.noreply.github.com>
2021-08-25 11:44:11 +0530

    Update Jenkinsfile-tier-0.groovy

--- pipeline/4/Jenkinsfile-tier-0.groovy
+++ pipeline/4/Jenkinsfile-tier-0.groovy
@@ -120,12 +120,13 @@ node(nodeName) {
     }
     
     stage('Testing') {
-        def result = sharedLib.getCIMessageMap()
-        println result
-        def base = "http://download.eng.bos.redhat.com/rhel-8/composes/auto/ceph-4.2-rhel-8/RHCEPH-4.2-RHEL-8-20210819.ci.0"
-        sharedLib.fetchCephVersion(base)
-//         def result = sharedLib.fetchMajorMinorOSVersion("compose")
+//         def result = sharedLib.getCIMessageMap()
 //         println result
+//         def base = "http://download.eng.bos.redhat.com/rhel-8/composes/auto/ceph-4.2-rhel-8/RHCEPH-4.2-RHEL-8-20210819.ci.0"
+//         sharedLib.fetchCephVersion(base)
+        def result = sharedLib.fetchMajorMinorOSVersion("compose")
+        println result
+        sharedLib.setLock(result["major_ver"], result["minor_ver"])
 
     }
 
