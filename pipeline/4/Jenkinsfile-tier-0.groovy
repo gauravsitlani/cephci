2502a8b82b03780aed686cc25b1063cad4c246f5
ckulal <83442614+ckulal@users.noreply.github.com>
2021-09-01 12:20:41 +0530

    Update Jenkinsfile-tier-0.groovy

--- pipeline/4/Jenkinsfile-tier-0.groovy
+++ pipeline/4/Jenkinsfile-tier-0.groovy
@@ -142,7 +142,7 @@ node(nodeName) {
 //     ]
 //         def topic = "VirtualTopic.qe.ci.rhcephqe.product-build.promote.complete"
 //         sharedLib.SendUMBMessageTest(msgMap, topic, "Tier0TestingDone")
-        def testResults = [ "01_deploy": "PASS", "02_object": "FAIL"]
+        def testResults = [ "01_deploy": "PASS", "02_object": "PASS"]
 //         sharedLib.sendEmailNew(testResults)
         sharedLib.sendGChatNotification(testResults)
     }
