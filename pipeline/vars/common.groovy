9c57ed5d9c267dbdfa872cf8f5d06fe8bc846828
ckulal <83442614+ckulal@users.noreply.github.com>
2021-09-01 09:44:00 +0530

    Update common.groovy

--- pipeline/vars/common.groovy
+++ pipeline/vars/common.groovy
@@ -36,40 +36,40 @@ def SendUMBMessageTest(def msgMap, def overrideTopic, def msgType){
 
 }
 
-@NonCPS
 def sendEmailNew(def testResults){
     def ciMsg = getCIMessageMap()
-    def jobStatus = "STABLE"
-    def version = ${ciMsg.artifact.nvr}.split("\\.|-")
-    def content = readFromReleaseFile(version[0], version[1], lockFlag=false)
-    def build_action = ciMsg["artifact"]["build_action"]
-    content[build_action]["repository"]
+    println ciMsg
+//     def jobStatus = "STABLE"
+//     def version = ${ciMsg.artifact.nvr}.split("\\.|-")
+//     def content = readFromReleaseFile(version[0], version[1], lockFlag=false)
+//     def build_action = ciMsg["artifact"]["build_action"]
+//     content[build_action]["repository"]
 
-    def body = readFile(file: "pipeline/vars/emailable-report.html")
-    body += "<h2><u>Test Artifacts</h2></u><table><tr><td> COMPOSE_URL </td><td>${ciMsg["build"]["compose-url"]}</td></tr><td>PRODUCT</td><td> ${ciMsg.artifact.name}</td></tr>"
-    body += "<tr><td> VERSION </td><td>${ciMsg.artifact.nvr}</td></tr>"
-    body += "<tr><td> CEPH-VERSION </td><td>${ciMsg.artifact.version}</td></tr>"
-    body += "<tr><td> REPOSITORY </td><td>${content[build_action]["repository"]}</td></tr>"
+//     def body = readFile(file: "pipeline/vars/emailable-report.html")
+//     body += "<h2><u>Test Artifacts</h2></u><table><tr><td> COMPOSE_URL </td><td>${ciMsg["build"]["compose-url"]}</td></tr><td>PRODUCT</td><td> ${ciMsg.artifact.name}</td></tr>"
+//     body += "<tr><td> VERSION </td><td>${ciMsg.artifact.nvr}</td></tr>"
+//     body += "<tr><td> CEPH-VERSION </td><td>${ciMsg.artifact.version}</td></tr>"
+//     body += "<tr><td> REPOSITORY </td><td>${content[build_action]["repository"]}</td></tr>"
 
-    body += "</table>"
-    body += "<body><u><h3>Test Summary</h3></u><br />"
-    def toList = "ckulal@redhat.com"
-    body += "<tr><th>Test Suite</th><th>Result</th>"
-    for (test in testResults) {
-        body += "<tr><td>${test.key}</td><td>${test.value}</td></tr>"
-        }
-    if ('FAIL' in testResults.values()){
-        jobStatus = "UNSTABLE"}
+//     body += "</table>"
+//     body += "<body><u><h3>Test Summary</h3></u><br />"
+//     def toList = "ckulal@redhat.com"
+//     body += "<tr><th>Test Suite</th><th>Result</th>"
+//     for (test in testResults) {
+//         body += "<tr><td>${test.key}</td><td>${test.value}</td></tr>"
+//         }
+//     if ('FAIL' in testResults.values()){
+//         jobStatus = "UNSTABLE"}
 
-    def subject = "Test report status of RH Ceph ${ciMsg.artifact.nvr} is ${jobStatus}"
+//     def subject = "Test report status of RH Ceph ${ciMsg.artifact.nvr} is ${jobStatus}"
 
-    emailext (
-        mimeType: 'text/html',
-        subject: "${subject}",
-        body: "${body}",
-        from: "cephci@redhat.com",
-        to: "${toList}"
-    )
+//     emailext (
+//         mimeType: 'text/html',
+//         subject: "${subject}",
+//         body: "${body}",
+//         from: "cephci@redhat.com",
+//         to: "${toList}"
+//     )
 }
 
 def prepareNode() {
