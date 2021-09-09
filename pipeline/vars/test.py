cd2e26a4fc37d80acaa645c40dcad545451cb3ab
ckulal <83442614+ckulal@users.noreply.github.com>
2021-09-09 15:55:39 +0530

    Update test.py

--- pipeline/vars/test.py
+++ pipeline/vars/test.py
@@ -1,8 +1,13 @@
-ceph_pkgs = requests.get(
-                    base_url + "/compose/Tools/x86_64/os/Packages/"
-                )
-                m = re.search(r"ceph-common-(.*?).x86", ceph_pkgs.text)
-                ceph_version.append(m.group(1))
-                m = re.search(r"ceph-ansible-(.*?).rpm", ceph_pkgs.text)
-                ceph_ansible_version.append(m.group(1))
-                log.info("Compose id is: " + compose_id)
+import requests
+
+def fetch_ceph_version(
+    base_url: str
+) -> None:
+   
+    ceph_pkgs = requests.get(
+      base_url + "/compose/Tools/x86_64/os/Packages/"
+    )
+    ceph_version = re.search(r"ceph-common-(.*?).x86", ceph_pkgs.text).group(1)
+    print(f"ceph_version is: {ceph_version}")
+
+  
