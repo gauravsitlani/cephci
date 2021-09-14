9039dd7e3e7fce53715d9414301be7bdd0b05200
anrao19 <83442624+anrao19@users.noreply.github.com>
2021-09-14 17:01:36 +0530

    Update test-rhceph-upgrade-rpm-on-rhel-7.sh

--- pipeline/scripts/4/2/tier-1/test-rhceph-upgrade-rpm-on-rhel-7.sh
+++ pipeline/scripts/4/2/tier-1/test-rhceph-upgrade-rpm-on-rhel-7.sh
@@ -11,13 +11,13 @@ test_conf="conf/nautilus/upgrades/tier_1_upgrade.yaml"
 test_inventory="conf/inventory/rhel-7.9-server-x86_64.yaml"
 return_code=0
 
-$WORKSPACE/.venv/bin/python run.py --v2 --osp-cred $HOME/osp-cred-ci-2.yaml --rhbuild $rhbuild --platform $platform --build $build_type --instances-name $instance_name --global-conf $test_conf --suite $test_suite --inventory $test_inventory --post-results --log-level DEBUG --report-portal
+echo "$WORKSPACE/.venv/bin/python run.py --v2 --osp-cred $HOME/osp-cred-ci-2.yaml --rhbuild $rhbuild --platform $platform --build $build_type --instances-name $instance_name --global-conf $test_conf --suite $test_suite --inventory $test_inventory --post-results --log-level DEBUG --report-portal"
 
 if [ $? -ne 0 ]; then
   return_code=1
 fi
 
-$WORKSPACE/.venv/bin/python run.py --cleanup $instance_name --osp-cred $HOME/osp-cred-ci-2.yaml --log-level debug
+echo "$WORKSPACE/.venv/bin/python run.py --cleanup $instance_name --osp-cred $HOME/osp-cred-ci-2.yaml --log-level debug"
 
 if [ $? -ne 0 ]; then
   echo "cleanup instance failed for instance $instance_name"
